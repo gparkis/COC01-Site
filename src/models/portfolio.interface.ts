6b057980d47b9e07d2f3e046fbc5ace655da8b56
Grant Parkis <gparkis@gmail.com>
2018-11-23 12:45:08 -0500

    second commit

--- src/models/portfolio.interface.ts
+++ src/models/portfolio.interface.ts
@@ -1,4 +1,15 @@
-export interface Portfolio
- {
+import {Social} from './social.interface';
+import {Changelog} from './changelog.interface';
+import {Updates} from './updates.interface';
 
+export interface Portfolio {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  subheading?: string;
+  socialMedia: Social[];
+  introduction: string;
+  changeLog: Changelog[];
+  updates: Updates[];
+  speakerBio: string;
  }
